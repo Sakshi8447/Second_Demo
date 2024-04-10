2ab2b4539119921354d32de579e83e0266378e5c
SakshiPathak <141705599+Sakshi8447@users.noreply.github.com>
2024-04-10 22:01:19 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,46 @@
+//Arthmatic operators
+console.log("hello");
+let a = 8;
+let b = 5;
+let c = a+b;
+console.log("a =", a);
+console.log("b =", b);
+console.log("a+b =" , a+b);
+console.log("a-b =", a-b);
+console.log("a*b =", a*b);
+console.log(c)
+
+//modulus operator
+console.log("a % b =", a % b);
+
+//exponentiation of a and b (8^5)
+console.log("a ** b =", a**b);
+
+// unary operator
+console.log("a =", a, "b =", b );
+a++;
+b++;
+a--;
+b--;
+console.log(a++, b--, b++, a--);
+
+//assignment operator
+let x = 5;
+let y = 2;
+
+x+=4;//a=a+4
+console.log("x =", x);
+
+x-=4; // x= x-4
+console.log("x =", x);
+
+// comparison operators
+
+console.log ("a==b", a==b);
+console.log("a!=b" , a!=b)
+
+
+
+console.log("a===b", a===b);
+
+console.log("a>b", a>b);
